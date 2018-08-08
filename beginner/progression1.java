0517fa627d513e8b4602277f0e7f4589e65b4b53
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-08-08 11:35:16 +0530

    Create progression1.java

---
+++ beginner/progression1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.*;
+class GFG
+{
+public static int Nth_of_AP(int a,intd,int N);
+{
+return(a +(N-1)*d);
+}
+public static void main(String args[])
+{
+int a=3;
+int d=1;
+int N=5
+System.out.println(Nth_of_AP(a,d,N));
+}
+}
