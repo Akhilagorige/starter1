62e4759206cca6e9afe0186798f310cdf746de55
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-08-13 12:26:40 +0530

    Create length3.java

---
+++ beginner/length3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class AssignmentProgramming {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        String str = input;
+        String[] myString = str.split(" ");
+        int length = str.length();
+        System.out.println(length);
+    }
+
+}
