ca9ddce82960065bbbee138c339cd9075ae5a74e
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-08-16 11:44:01 +0530

    Create swap4.java

---
+++ beginner/swap4.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int  m=s.nextInt();
+	if(n<m)
+	{
+		int temp=n;
+		n=m;
+		m=temp;// your code goes here
+	}
+	System.out.println(n+" "+m);
+}
+}
