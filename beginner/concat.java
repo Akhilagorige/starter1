80d7dc643203db23e6748a942158fb29a115afff
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-08-17 10:19:32 +0530

    Create concat.java

---
+++ beginner/concat.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc = new Scanner(System.in);
+		
+		/* Display message for user to take first 
+		string input from keyboard */
+		
+		String firstStr = sc.next();
+		
+		/* Display message for user to take first 
+		string input from keyboard */
+		
+		String secondStr = sc.next();
+		
+		/* Display message for displaying result */
+		
+		
+		/* '+' operator concatenate string */
+		System.out.println(firstStr+ "" + secondStr);	// your code goes here
+	}
+}
