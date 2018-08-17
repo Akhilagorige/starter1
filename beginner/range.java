5a8068596c1689ea8fc76e4b8582ccbf6f9d791c
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-08-17 10:48:31 +0530

    Create range.java

---
+++ beginner/range.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=1;
+		if(1<n && n<10)
+		System.out.println("yes");
+		else
+		System.out.println("No");// your code goes here
+	}
+}
