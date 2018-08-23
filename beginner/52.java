678a4854536652402f793cc3da1d09a64afffed0
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-08-23 10:15:25 +0530

    Create 52.java

---
+++ beginner/52.java
@@ -0,0 +1,48 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n;
+	Scanner s=new Scanner(System.in);
+	n=s.nextInt();
+	switch(n)
+	{
+	case 1:System.out.println("one");
+	break;
+	case 2:System.out.println("two");
+	break;// your code goes here
+	
+	case 3:System.out.println("three");
+	break;// your code goes here
+	
+	case 4:System.out.println("four");
+	break;// your code goes here
+	
+	case 5:System.out.println("five");
+	break;// your code goes here
+	
+	case 6:System.out.println("six");
+	break;// your code goes here
+	
+	case 7:System.out.println("seven");
+	break;// your code goes here
+	
+	case 8:System.out.println("eight");
+	break;// your code goes here
+	
+	case 9:System.out.println("nine");
+	break;// your code goes here
+	
+	case 10:System.out.println("ten");
+	break;// your code goes here
+	default:System.out.println(" ");// your code goes here
+	}
+	}
+}
