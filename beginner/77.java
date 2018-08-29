4409edbc61b35b1091b836ff87cc7d578e78e234
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-08-29 10:16:21 +0530

    Update 77.java

--- beginner/77.java
+++ beginner/77.java
@@ -16,7 +16,7 @@ class Ideone
         
         for(int i = 1; i <= n; ++i) {
             if (n % i == 0) {
-                System.out.print(i + " ");
+                System.out.print(i+ " ");
             }
         }	// your code goes here
 	}
