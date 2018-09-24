45c5be5a07f2ebbc166e2c7420b5fa43ef3e1787
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-09-24 15:55:31 +0530

    Create 54a.java

---
+++ beginner/54a.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n;
+     int sum=0;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     if(n%2!=0){
+         System.out.print(n-1);
+     }
+     else
+     {
+     	System.out.println(n);
+     }// your code goes here
+	}
+}
