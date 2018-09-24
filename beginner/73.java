60ea36ba420be824ea390d34ef455bd8aa47f173
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-09-24 10:46:45 +0530

    Create 73.java

---
+++ beginner/73.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int a;
+         int f=0;
+         int b,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    b=s.nextInt();
+    c=s.nextInt();
+    for(int i=b;i<=c;i++){
+        if(i==a){
+            f=1;
+            break;
+        }
+    }
+   if(f==1){
+       System.out.println("yes");
+   }
+ else{
+        System.out.println("no");
+ }
+
+	}
+}
