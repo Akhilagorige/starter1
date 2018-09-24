f0e22a639b05b1bad11516463a54b6aa3387493d
Akhilagorige <41730905+Akhilagorige@users.noreply.github.com>
2018-09-24 12:42:10 +0530

    Create 81.java

---
+++ beginner/81.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			int a,b,i,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=b-a;
+		System.out.println(c);
+	// your code goes here
+	}
+}
